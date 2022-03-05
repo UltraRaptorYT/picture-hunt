650499e513b3fe6846d3fd840a23dcd6af044636
UltraRaptorYT <62703912+UltraRaptorYT@users.noreply.github.com>
2022-03-05 20:45:54 +0800

    Remove unnecessary info

--- client/src/App.js
+++ client/src/App.js
@@ -3,12 +3,14 @@ import "./App.css";
 import { Route } from "react-router-dom";
 import Homepage from "./components/home";
 import Game from "./components/Game";
+import gameOver from "./components/gameOver";
 
 const App = () => {
   return (
     <div className="App">
       <Route path="/" exact component={Homepage} />
       <Route path="/play" exact component={Game} />
+      <Route path="/gameOver" exact component={gameOver} />
     </div>
   );
 };
